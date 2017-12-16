2e212b5f9ff84f0d0d678e782fea7fa4d00ad1c7
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-16 21:20:32 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,20 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.size
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.songs.count(:artist_id)
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    artist_ids = []
+    self.songs.each do |s|
+      artist_ids << s.artist_id
+    end.uniq
+    Artist.where(id:artist_ids).pluck(:name)
   end
 end
